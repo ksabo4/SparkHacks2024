f1849772d17040163542bbd15fb1dc2b657397db
Kyle Sabo <ksabo4@uic.edu>
2024-02-10 21:33:14 +0000

    added investments

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,25 +1,18 @@
-
-
 public class Character {
     private double money;
     private int age;
     private int happiness;
-    private double investments;
-    private String job;
-    private boolean college;
+    private double[] investments = {0.0, 0.0, 0.0};
+    private double goal;
 
     Character() {
         money = 0.0;
-        age = 5;
+        age = 18;
         happiness = 50;
-        investments = 0.0;
-        job = "";
-        college = false;
+        goal = 100000;
     }
-    void addMoney() {
-        if(age < 18) {
-            money += 100;
-        }
+    void annualIncome(int amount) {
+        money += amount;
     }
 
     double getMoney() {
@@ -35,41 +28,77 @@ public class Character {
     }
 
     double getInvestments() {
-        return investments;
+        return investments[0] + investments[1] + investments[2];
     }
 
-    String getJob() {
-        return job;
+    double getGoal() {
+        return goal;
     }
 
-    void progressTime() {
+    void progressTime(int amount) {
         age++;
-        addMoney();
-        investments *= 1.1;
+        annualIncome(amount);
+
+        int counter = 0;
+        for (double investment : investments) {
+            double rate = Math.random() * (1.5 - 0.5) + 0.6;
+            investment *= rate;
+            investments[counter] = investment;
+            counter += 1;
+        }
     }
-    
-    void invest(double userInvest) {
-        investments += userInvest;
+
+    void spend(int amount)
+    {
+        money -= amount;
+    }
+
+    void invest(double userInvest, int risk) {
+        if (userInvest > money) {
+            return;
+        }
+        investments[risk] += userInvest;
         money -= userInvest;
     }
 
-    void withdrawInvestments(double withdrawAmount) {
-        if(withdrawAmount > investments) {
+    void withdrawInvestments(double withdrawAmount, int risk) {
+        if(withdrawAmount > investments[risk]) {
             return;
         }
-        investments -= withdrawAmount;
+        investments[risk] -= withdrawAmount;
         money += withdrawAmount;
+
+        if (withdrawAmount > 10000) {
+            happiness += 10;
+            if (happiness > 100) {
+                happiness = 100;
+            }
+        }
     }
 
     void gamble(int userGamble) {
-        money = money - userGamble;
+        money -= userGamble;
         int rand = (int)(Math.random() * 100);
 
         if(rand == 100) {
             money += userGamble * 5;
+            happiness += 20;
         }
         else if(rand > 80 && rand < 99) {
             money += userGamble;
+            happiness += 5;
         }
+        else {
+            happiness -= 30;
+        }
+        if (happiness < 0) {
+            happiness = 0;
+        } else if (happiness > 100) {
+            happiness = 100;
+        }
+    }
+    void work(float amount) {
+        happiness -= 5;
+        money += amount;
     }
 }
--- src/main/java/ScreenController.java
+++ src/main/java/ScreenController.java
@@ -3,15 +3,91 @@
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
 
-public class ScreenController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ScreenController implements Initializable {
+    private Character player;
+    private int income;
     @FXML
     private Label gameText;
+
     @FXML
-    private void gameText() throws IOException {
-        System.out.println("Clicked!");
-        gameText.setText("New Text");
+    private Label moneyText;
+    @FXML
+    private Label investmentText;
+    @FXML
+    private Label happinessText;
+    @FXML
+    private Label ageText;
+    @FXML
+    private Label goalText;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        player = new Character();
+        income = 3000;
+    }
+
+    @FXML
+    private void buttonPressed(ActionEvent event) throws IOException {
+        String buttonNum = (String)((Node) event.getSource()).getUserData();
+        if (buttonNum.equals("1"))
+        {
+            player.invest(100, 1);
+        }
+        else if (buttonNum.equals("2"))
+        {
+            player.gamble(5);
+        }
+        else if (buttonNum.equals("3"))
+        {
+            player.spend(1000);
+        }
+        else if (buttonNum.equals("4"))
+        {
+            player.work(90);
+        }
+        else if (buttonNum.equals("5"))
+        {
+            player.gamble(100);
+        }
+        else if (buttonNum.equals("6"))
+        {
+            player.invest(50, 0);
+        }
+        else if (buttonNum.equals("7"))
+        {
+            player.progressTime(income);
+        }
+        else if (buttonNum.equals("8"))
+        {
+            player.withdrawInvestments(1000,1);
+        }
+        else if (buttonNum.equals("9"))
+        {
+            player.withdrawInvestments(1000,0);
+        }
+        moneyText.setText("Money: $" + player.getMoney());
+        investmentText.setText("Investments: $" + player.getInvestments());
+        happinessText.setText("Happiness: " + player.getHappiness());
+        ageText.setText("Age: " + player.getAge() + " Years");
     }
 
     @FXML
